48b72e1cd3b77fafe4d0dd1dd5c7238ffbe637b8
sachin dave <davesachin26@gmail.com>
2024-02-18 00:37:56 +0530

    java8 practice

--- src/mutithreading/MultiLockSync.java
+++ src/mutithreading/MultiLockSync.java
@@ -3,6 +3,7 @@ package mutithreading;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Random;
 
 public class MultiLockSync {
 
@@ -10,8 +11,10 @@ public class MultiLockSync {
      * common method(dowork1,dowork2) which is shared by  common thread process t1,t2 , if it's doing manipulation on different object rather then making method
      * synchronized we should make object synchronized to make performace better
      */
-    List<Integer> list1 = new ArrayList<>();
-    List<Integer> list2 = new ArrayList<>();
+
+    Random r = new Random();
+    private List<Integer> list1 = new ArrayList<>();
+    private List<Integer> list2 = new ArrayList<>();
 
     private Object lock1 = new Object();
     private Object lock2 = new Object();
@@ -19,24 +22,22 @@ public class MultiLockSync {
         Long startTime = System.currentTimeMillis();
         Thread t1 = new Thread(() -> {
             try {
-                for(int i = 0;i <1000; i++){
-                    doWork1();
-                    doWork2();
-                }
+                doWork();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         });
 
+        t1.start();
+
         Thread t2 = new Thread(() -> {
             try {
-                doWork1();
-                doWork2();
+              doWork();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         });
-        t1.start();
+
         t2.start();
         t1.join();
         t2.join();
@@ -46,30 +47,38 @@ public class MultiLockSync {
         System.out.println("total time :"+(endTime-startTime));
 
         System.out.println("list1 size"+list1.size());
-        System.out.println("list1 size"+list2.size());
+        System.out.println("list2 size"+list2.size());
 
 
     }
+
+    private void doWork() throws InterruptedException {
+        for(int i = 0;i <1000; i++){
+            doWork1();
+            doWork2();
+        }
+    }
+
     //public synchronized void doWork() throws InterruptedException { // it will work but will impact performance
         public  void doWork1() throws InterruptedException { // it will work but will impact performance
-            synchronized (list1) {
+            synchronized (lock1) {
                     try {
                         Thread.sleep(1);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
-                    list1.add(1);
+                    list1.add(r.nextInt(100));
                 }
             }
 
     public  void doWork2() throws InterruptedException { // it will work but will impact performance
-        synchronized (list2) {
+        synchronized (lock2) {
                 try {
                     Thread.sleep(1);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-                list2.add(1);
+                list2.add(r.nextInt(100));
         }
     }
     public static void main(String[] args) throws InterruptedException {
